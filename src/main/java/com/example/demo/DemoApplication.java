d781a1e968321b62615c7ac76bcd2da8dc8c6722
Kohei Hashimoto <hashimoto.kohei@linecorp.com>
2023-08-17 10:09:33 +0900

    sample change

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,7 +16,8 @@ public class DemoApplication {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+		System.out.println(name);
+		return String.format("Hello %s!", name);
     }
 
 }
